acd68ae3c8824902fdf0bf7e621b76a01b2fcc87
kweku-xvi <nksarps@gmail.com>
2023-10-01 16:20:59 +0000

    added a new view for courier

--- main/views.py
+++ main/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
-from .forms import EmailSender
+from .forms import EmailSender, EmailSenderCourier
+from trycourier import Courier
 
+# using courier 
 def email(request):
+    if request.method == 'POST':
+        form = EmailSenderCourier(request.POST)
+
+        if form.is_valid():
+            recipient_email = form.cleaned_data['recipient_mail']
+            message = form.cleaned_data['message']
+
+            client = Courier(auth_token="pk_prod_P6KGDK2MFM44YHQYXE0TMXQE1BW0")
+
+            resp = client.send_message(
+                message={
+                    "to": {
+                    "email": recipient_email,
+                    },
+                    "template": "VVY5W4MWN840MHKGKJBCRRDXC18S",
+                    "data": {
+                    "message": message,
+                    },
+                }
+            )
+
+            return redirect('complete')
+    else:
+        form = EmailSenderCourier()
+
+    return render(request, 'main/home_courier.html', {'form':form})
+
+
+def complete(request):
+    return render(request, 'main/complete.html')
+
+
+# using SMTP
+def email_smtp(request):
     if request.method == 'POST':
         form = EmailSender(request.POST)
 
@@ -20,8 +56,4 @@ def email(request):
             return redirect('complete')
     else:
         form = EmailSender()
-    return render(request, 'main/home.html', {'form':form})
-
-
-def complete(request):
-    return render(request, 'main/complete.html')
+    return render(request, 'main/home_smtp.html', {'form':form})
