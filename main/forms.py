0b443a991719ba1a80a63430d02b9569eaee0ae4
kweku-xvi <nksarps@gmail.com>
2023-10-01 16:20:19 +0000

    added another form for courier

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
+# form when using SMTP
 class EmailSender(forms.Form):
     sender_mail = forms.EmailField(label="Sender's Email")
     recipient_mail = forms.EmailField(label="Recipient's Email")
     subject = forms.CharField(max_length=1000)
+    message = forms.CharField(widget=forms.Textarea)
+
+class EmailSenderCourier(forms.Form):
+    recipient_mail = forms.EmailField(label="Recipient's Email")
     message = forms.CharField(widget=forms.Textarea)
